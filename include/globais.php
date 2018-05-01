4794763d27b658369003fd373f3788b451cccdd2
babirondo <babirondo@gmail.com>
2018-05-01 21:46:52 +0100

    preparando para rodar no servidor e local

--- include/globais.php
+++ include/globais.php
@@ -6,35 +6,49 @@ set_time_limit(2);
 class Globais{
 
     function __construct( ){
+        $env = "prod";
+        //$env = "local";
 
-        $this->Authentication_endpoint = "http://localhost:81/PaintballSocialNetwork-AuthAPI/Auth/";
+        switch($env){
+            case("local");
+                $servidor= "http://localhost:81";
+            break;
+
+            case("prod");
+                $servidor= "http://pb.mundivox.rio";
+            break;
+
+        }
+
+        
+        $this->Authentication_endpoint = $servidor."/PaintballSocialNetwork-AuthAPI/Auth/";
 
 
-        $this->ROTA_RAIZ = "http://localhost:81/PaintballSocialNetwork";
-        $this->Players_UPDATE_endpoint = "http://localhost:81/PaintballSocialNetwork-Players/Players/:idjogadorlogado";
-        $this->Players_GET_endpoint = "http://localhost:81/PaintballSocialNetwork-Players/Players/:idjogadorlogado";
-        $this->Players_ADD_TEAM_endpoint = "http://localhost:81/PaintballSocialNetwork-Players/Players/Experiences/";
-        $this->listar_times_de_um_jogador = "http://localhost:81/PaintballSocialNetwork-Players/Players/:idjogadorlogado/Experiences";
-        $this->excluir_experiencia = "http://localhost:81/PaintballSocialNetwork/MyProfile/Experiences/:idexperiencia";
-        $this->delete_experiencia = "http://localhost:81/PaintballSocialNetwork-Players/Players/:idjogadorlogado/Experiences/:idexperiencia";
+        $this->ROTA_RAIZ = $servidor."/PaintballSocialNetwork";
+        $this->Players_UPDATE_endpoint = $servidor."/PaintballSocialNetwork-Players/Players/:idjogadorlogado";
+        $this->Players_GET_endpoint = $servidor."/PaintballSocialNetwork-Players/Players/:idjogadorlogado";
+        $this->Players_ADD_TEAM_endpoint = $servidor."/PaintballSocialNetwork-Players/Players/Experiences/";
+        $this->listar_times_de_um_jogador = $servidor."/PaintballSocialNetwork-Players/Players/:idjogadorlogado/Experiences";
+        $this->excluir_experiencia = $servidor."/PaintballSocialNetwork/MyProfile/Experiences/:idexperiencia";
+        $this->delete_experiencia = $servidor."/PaintballSocialNetwork-Players/Players/:idjogadorlogado/Experiences/:idexperiencia";
 
 
-        $this->MeusTimes = "http://localhost:81/PaintballSocialNetwork/MySquads/";
-        $this->CriarMeuTime = "http://localhost:81/PaintballSocialNetwork/MySquads/New/";
-        $this->CriarMeuTimeSalvar = "http://localhost:81/PaintballSocialNetwork-Players/:idjogadorlogado/Teams/";
-        $this->MeusTimesRemoto = "http://localhost:81/PaintballSocialNetwork-Players/:idjogadorlogado/MySquads/";
+        $this->MeusTimes = $servidor."/PaintballSocialNetwork/MySquads/";
+        $this->CriarMeuTime = $servidor."/PaintballSocialNetwork/MySquads/New/";
+        $this->CriarMeuTimeSalvar = $servidor."/PaintballSocialNetwork-Players/:idjogadorlogado/Teams/";
+        $this->MeusTimesRemoto = $servidor."/PaintballSocialNetwork-Players/:idjogadorlogado/MySquads/";
 
-        $this->listar_meus_times = "http://localhost:81/PaintballSocialNetwork-Players/MyTeams/:idjogadorlogado";
-        $this->jogadores_por_times = "http://localhost:81/PaintballSocialNetwork-Players/Teams/Players/";
-        $this->getTimes = "http://localhost:81/PaintballSocialNetwork-Players/Teams/";
-        $this->NewUser_endpoint_UI = "http://localhost:81/PaintballSocialNetwork/NewUser/";
-        $this->NewUser_endpoint = "http://localhost:81/PaintballSocialNetwork-AuthAPI/NewUser/";
-        $this->ProcurarTimesUI = "http://localhost:81/PaintballSocialNetwork/SearchTeams/";
-        $this->ProcurarTimes = "http://localhost:81/PaintballSocialNetwork-Players/SearchTeams/";
+        $this->listar_meus_times = $servidor."/PaintballSocialNetwork-Players/MyTeams/:idjogadorlogado";
+        $this->jogadores_por_times = $servidor."/PaintballSocialNetwork-Players/Teams/Players/";
+        $this->getTimes = $servidor."/PaintballSocialNetwork-Players/Teams/";
+        $this->NewUser_endpoint_UI = $servidor."/PaintballSocialNetwork/NewUser/";
+        $this->NewUser_endpoint = $servidor."/PaintballSocialNetwork-AuthAPI/NewUser/";
+        $this->ProcurarTimesUI = $servidor."/PaintballSocialNetwork/SearchTeams/";
+        $this->ProcurarTimes = $servidor."/PaintballSocialNetwork-Players/SearchTeams/";
 
-        $this->MyProfileUI = "http://localhost:81/PaintballSocialNetwork/MyProfile/";
-        $this->LogoutUI = "http://localhost:81/PaintballSocialNetwork/Logout/";
-        $this->LoginUI = "http://localhost:81/PaintballSocialNetwork/Login";
+        $this->MyProfileUI = $servidor."/PaintballSocialNetwork/MyProfile/";
+        $this->LogoutUI = $servidor."/PaintballSocialNetwork/Logout/";
+        $this->LoginUI = $servidor."/PaintballSocialNetwork/Login";
 
 
 
